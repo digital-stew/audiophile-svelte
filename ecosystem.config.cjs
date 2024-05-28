1114c2267ffbe60902cf8c188d316dfba4ea67a3
Stewart Ridings <stewart@tux-systems.co.uk>
2024-05-28 05:43:04 +0100

    pm2 setup

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,31 +1,32 @@
 module.exports = {
-    /**
-     * Application configuration section
-     * http://pm2.keymetrics.io/docs/usage/application-declaration/
-     */
-    apps: [
-        // First application
-        {
-            name: 'website.com', // name of the process in PM2
-            script: 'build/index.js',
-            env_production: {
-                NODE_ENV: 'production',
-                PORT: 10004 // port the app will be launched on
-            }
-        }
-    ],
+	/**
+	 * Application configuration section
+	 * http://pm2.keymetrics.io/docs/usage/application-declaration/
+	 */
+	apps: [
+		// First application
+		{
+			name: 'website.com', // name of the process in PM2
+			script: 'build/index.js',
+			PORT: 10004,
+			env: {
+				NODE_ENV: 'production',
+				PORT: 10004 // port the app will be launched on
+			}
+		}
+	],
 
-    /**
-     * Deployment section
-     * http://pm2.keymetrics.io/docs/usage/deployment/
-     */
-    deploy: {
-        production: {
-            user: 'deployer', // deployer user
-            host: '127.0.0.1', // IP address of your server
+	/**
+	 * Deployment section
+	 * http://pm2.keymetrics.io/docs/usage/deployment/
+	 */
+	deploy: {
+		production: {
+			user: 'deployer', // deployer user
+			host: '127.0.0.1', // IP address of your server
 
-            path: '/var/node/audiophile-svelte', // the path where you want the project to be
-            // code you want to run after the project has been pushed to your server
-        }
-    }
+			path: '/var/node/audiophile-svelte' // the path where you want the project to be
+			// code you want to run after the project has been pushed to your server
+		}
+	}
 };
--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import path from 'path'
+import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
